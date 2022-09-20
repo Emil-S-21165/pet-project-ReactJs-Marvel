08208955ecee71a9a195696c2e84b27841702f18
EmilS <emil.s21165@gmail.com>
2022-09-20 14:54:14 +0600

    show comics for characters function

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -35,7 +35,7 @@ class CharList extends Component{
     }
 
     renderItems(arr) {
-        const items =  arr.map((item, id) => {
+        const items =  arr.map((item) => {
             let imgStyle = {'objectFit' : 'cover'};
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 imgStyle = {'objectFit' : 'unset'};
@@ -44,7 +44,8 @@ class CharList extends Component{
             return (
                 <li 
                     className="char__item"
-                    key={id}>
+                    key={item.id}
+                    onClick={() => this.props.onCharSelected(item.id)}>
                         <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                         <div className="char__name">{item.name}</div>
                 </li>
